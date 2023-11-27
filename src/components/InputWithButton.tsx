3b3d07e3cbba2b798d93803db8862fdca8172802
enas48 <enasm4829@gmail.com>
2023-11-27 16:55:36 +0200

    add header and footer

---
+++ src/components/InputWithButton.tsx
@@ -0,0 +1,35 @@
+import React,{useState} from "react";
+import { Input, Button } from "@material-tailwind/react";
+interface Target {
+  target: {
+    value:string
+  };
+}
+ 
+export function InputWithButton() {
+  const [email, setEmail] =useState<string>("");
+  const onChange = ({ target }:Target) => setEmail(target.value);
+ 
+  return (
+    <div className="relative flex w-full max-w-[24rem]">
+      <Input
+        type="email"
+        label="Email Address"
+        value={email}
+        onChange={onChange}
+        className="pr-20"
+        containerProps={{
+          className: "min-w-0",
+        }}
+      />
+      <Button
+        size="sm"
+        color={email ? "gray" : "blue-gray"}
+        disabled={!email}
+        className="!absolute right-1 top-1 rounded"
+      >
+        Invite
+      </Button>
+    </div>
+  );
+}

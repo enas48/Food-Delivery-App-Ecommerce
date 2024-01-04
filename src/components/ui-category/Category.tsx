5897ae4d4425099a806b33a8dd9cab26573ab378
enas48 <enasm4829@gmail.com>
2024-01-04 13:00:27 +0200

    a

--- src/components/ui-category/Category.tsx
+++ src/components/ui-category/Category.tsx
@@ -2,17 +2,29 @@ import React from 'react'
 
 export default function Category() {
     return (
-        <div className='grid grid-cols-4 gap-4'>
+        <div className='grid grid-cols-4 gap-2 md:gap-4 justify-items-center  text-center  content-center'>
 
-<span>
-<i className="fa-solid fa-burger text-center w-[4rem] h-[4rem] leading-[4rem] bg-[#FB9C16] text-4xl rounded-full"></i>
-<span></span>
+            <span className='flex flex-col md:flex-row w-full items-center text-center md:text-left p-2 content-center  bg-[#fff8eb] gap-2 transition ease-in-out delay-100 hover:-translate-y-2 hover:scale-105  duration-300 cursor-pointer'>
+                <i className="fa-solid fa-burger text-center w-[3rem] h-3rem] leading-[3rem]  text-3xl md:w-[4rem] md:h-[4rem] md:leading-[4rem]  md:text-4xl bg-[#FB9C16] rounded-full"></i>
+                <span>Fastfood</span>
 
-</span>
+            </span>
+            <span className='flex flex-col md:flex-row w-full items-center text-center md:text-left p-2 content-center  bg-[#fff8eb] gap-2 transition ease-in-out delay-100 hover:-translate-y-2 hover:scale-105  duration-300 cursor-pointer'>
+                <i className="fa-solid fa-pizza-slice text-center w-[3rem] h-3rem] leading-[3rem]  text-3xl md:w-[4rem] md:h-[4rem] md:leading-[4rem]  md:text-4xl bg-[#FB9C16]  rounded-full"></i>
+                <span>Pizza</span>
 
-<i className="fa-solid fa-pizza-slice text-center w-[4rem] h-[4rem] leading-[4rem]  bg-[#FB9C16] text-4xl rounded-full"></i>
-<i className="fa-solid fa-bowl-rice text-center w-[4rem] h-[4rem] leading-[4rem] bg-[#FB9C16] text-4xl  rounded-full"></i>
-<i className="fa-solid fa-bacon text-center w-[4rem] h-[4rem] leading-[4rem] bg-[#FB9C16] text-4xl rounded-full"></i>
+            </span>
+            <span className='flex flex-col md:flex-row w-full items-center text-center md:text-left p-2 content-center  bg-[#fff8eb] gap-2 transition ease-in-out delay-100 hover:-translate-y-2 hover:scale-105  duration-300 cursor-pointer'>
+
+                <i className="fa-solid fa-bowl-rice text-center w-[3rem] h-3rem] leading-[3rem]  text-3xl md:w-[4rem] md:h-[4rem] md:leading-[4rem]  md:text-4xl bg-[#FB9C16]  rounded-full"></i>
+                <span>Asian food</span>
+            </span>
+            <span className='flex flex-col md:flex-row w-full items-center text-center md:text-left p-2 content-center  bg-[#fff8eb] gap-2 transition ease-in-out delay-100 hover:-translate-y-2 hover:scale-105  duration-300 cursor-pointer'>
+
+                <i className="fa-solid fa-bacon text-center w-[3rem] h-3rem] leading-[3rem]  text-3xl md:w-[4rem] md:h-[4rem] md:leading-[4rem]  md:text-4xl bg-[#FB9C16]  rounded-full"></i>
+                <span>Meat</span>
+
+            </span>
         </div>
     )
 }

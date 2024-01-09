d140b86f0c3f97e26f07dee5eb52e88835a334a8
enas48 <enasm4829@gmail.com>
2024-01-09 16:56:40 +0200

    a

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Product } from '../../types/Product'
-export default function ProductCard(props:{item:Product}) {
+export default function ProductCard(props: { item: Product }) {
     const { id,
         title,
         imageUrl,
         price,
         category } = props.item
     return (
-        <div className=''>
-            <div className=''>
-                <img src={imageUrl} className='w-[4rem]' alt='product-img' />
+        <div className='shadow-md p-3 rounded m-2 items-center flex flex-col gap-3 '>
+            <div className='transition ease-in-out delay-100 hover:-translate-y-2 hover:scale-105  duration-300'>
+                <Link to={`/foods/${id}`}>  <img src={imageUrl} className='h-[4rem]' alt='product-img' /></Link>
             </div>
-            <div className=''>
-                <h5>
-                    <Link to='/'>{title}</Link>
-                </h5>
-                <div>
-                    <span>${price}</span>
-                    <button className=''>Add to Cart</button>
-                </div>
-                <div>
+            <h5 className='font-medium'>
+                <Link to={`/foods/${id}`}>{title}</Link>
+            </h5>
 
-                </div>
+            <div className='flex justify-between items-center gap-6 flex-wrap'>
+                <span className='font-bold'>${price}</span>
+                <button className='bg-[#FB9C16] text-white text-nowrap rounded p-2   m-2'>Add to Cart</button>
             </div>
+
+
 
         </div>
     )
